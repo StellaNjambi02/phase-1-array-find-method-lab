faf5d12bfce80f420d8241f7d1c61232121adbf4
StellaNjambi02 <njambindungistella@gmail.com>
2024-07-01 20:38:57 +0300

    array find method

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function superbowlWin(array){
+    for(const user of array){ 
+    if(user.result==="W"){
+        return user.year;
+    }
+    
+}}
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+  record.find(superbowlWin);
+
+  
